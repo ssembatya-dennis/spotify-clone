87a7845737e65b6976a335c7d524c519bde741f0
DESKTOP-G60BJ3E\Ssonko Jimmy <ssembatyadennis25@gmail.com>
2022-09-17 08:52:37 +0300

    Implement the sign up page version one

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { SideArticle, MainStream, Article } from "./containers/Containers";
+// import {
+//   SideArticle,
+//   MainStream,
+//   Article,
+//   Media,
+// } from "./containers/Containers";
+import SignUp from "./layout/sign-up/SignUp";
 
 class App extends React.Component {
   render() {
     return (
-      <main className="app-container">
-        <SideArticle />
-        <MainStream />
-        <Article />
-      </main>
+      <>
+        {/* <main className="app-container">
+          <SideArticle />
+          <MainStream />
+          <Article />
+          <Media />
+        </main> */}
+        <SignUp />
+      </>
     );
   }
 }
